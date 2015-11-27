a3dd01677c20bd74fa10f7b86e262fbee10b0052
Ash <ssetem@googlemail.com>
2015-11-27 12:51:34 +0000

    add search reset to Pagination

--- src/domain/accessors/PaginationAccessor.ts
+++ src/domain/accessors/PaginationAccessor.ts
@@ -3,6 +3,10 @@ import RootBuilder from "../builders/RootBuilder.ts";
 
 export default class PaginationAccessor extends Accessor{
 
+  searchReset(){
+    this.state.clear()
+  }
+
   buildPostQuery(builder:RootBuilder, page) {
     let from = builder.size * (page - 1)
     builder.setFrom(from);
