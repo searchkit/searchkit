5ffd62c8add82135a8718e2fd5236ef1a4f7ce3b
Ash <ssetem@googlemail.com>
2015-12-07 14:52:12 +0000

    disable encode

--- src/core/history.ts
+++ src/core/history.ts
@@ -1,2 +1,10 @@
 import { createHistory, useQueries } from 'history'
-export const history = useQueries(createHistory)({})
+const qs = require("qs")
+export const history = useQueries(createHistory)({
+  stringifyQuery(ob){
+    return qs.stringify(ob, {encode:false})
+  },
+  parseQueryString(str){
+    return qs.parse(str)
+  }
+})
