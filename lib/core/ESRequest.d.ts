58d49f30173d9d7d3332b6ad6a4d6fe46203ef58
Ash <ssetem@googlemail.com>
2015-12-07 16:15:01 +0000

    encode for now

--- lib/core/ESRequest.d.ts
+++ lib/core/ESRequest.d.ts
@@ -2,5 +2,5 @@ export declare class ESRequest {
     index: string;
     constructor(index: string);
     searchUrl(): string;
-    search(queries: any): Promise<{}>;
+    search(query: any): Promise<{}>;
 }
