a34fd25688de7f9f055638a6112e8be8f03102c3
Ash <ssetem@googlemail.com>
2015-12-23 16:03:33 +0000

    tell karma not to create sub dir

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       outputDir: '$CIRCLE_TEST_REPORTS/junit', // results will be saved as $outputDir/$browserName.xml
       outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true // add browser name to report and classes names
+      useBrowserName: false // add browser name to report and classes names
     },
 
     webpack: new WebpackConfig().extend("webpack.test.config")
