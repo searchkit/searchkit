173b84560e471975f82958568f22f45f8c77a977
Joseph McElroy <phoey1@gmail.com>
2015-12-30 09:52:00 +0000

    update search box to work under white background

--- lib/components/search/search-box/src/SearchBox.d.ts
+++ lib/components/search/search-box/src/SearchBox.d.ts
@@ -1,59 +1,22 @@
-import { SearchAccessor, SearchkitComponent, ESTransport } from "../../../../core";
+import { SearchAccessor, SearchkitComponent } from "../../../../core";
 export interface ISearchBox {
     searchOnChange?: boolean;
-    autocomplete?: boolean;
     prefixQueryFields?: Array<string>;
     queryFields?: Array<string>;
     mod?: string;
 }
 export declare class SearchBox extends SearchkitComponent<ISearchBox, any> {
     accessor: SearchAccessor;
-    suggestSearcher: ESTransport;
     constructor(props: ISearchBox);
+    componentWillMount(): void;
     defineBEMBlocks(): {
         container: string;
     };
-    componentWillMount(): void;
     defineAccessor(): SearchAccessor;
     onSubmit(event: any): void;
     searchQuery(query: any): void;
-    processSuggestions(results: any): {
-        sectionName: string;
-        suggestions: any[];
-    }[];
-    getSuggestionQueryObject(query: any): {
-        size: number;
-        suggest: {
-            text: any;
-            "suggestions": {
-                "phrase": {
-                    field: string;
-                    "real_word_error_likelihood": number;
-                    "max_errors": number;
-                    "gram_size": number;
-                    "direct_generator": {
-                        "field": string;
-                        "suggest_mode": string;
-                        "min_word_length": number;
-                    }[];
-                    "highlight": {
-                        "pre_tag": string;
-                        "post_tag": string;
-                    };
-                };
-            };
-            "completion": {
-                completion: {
-                    field: string;
-                };
-            };
-        };
-    };
-    querySuggestions(query: any, callback: any): void;
-    suggestionRenderer(suggestion: any, input: any): JSX.Element;
     getValue(): string;
     onChange(e: any): void;
-    suggestionValue(suggestion: any): any;
-    onSuggestionSelected(value: any): void;
+    setFocusState(focused: boolean): void;
     render(): JSX.Element;
 }
--- src/components/search/search-box/src/SearchBox.tsx
+++ src/components/search/search-box/src/SearchBox.tsx
@@ -7,35 +7,32 @@ import "../styles/index.scss";
 
 import {
 	SearchAccessor,
-	SearchkitComponent,
-	ESTransport
+	SearchkitComponent
 } from "../../../../core"
 
 export interface ISearchBox {
 	searchOnChange?:boolean
-	autocomplete?:boolean
 	prefixQueryFields?:Array<string>
 	queryFields?:Array<string>
 	mod?:string
 }
 
-
-
 export class SearchBox extends SearchkitComponent<ISearchBox, any> {
 	accessor:SearchAccessor
-	suggestSearcher: ESTransport
 
 	constructor (props:ISearchBox) {
 		super(props);
-	}
-
-	defineBEMBlocks() {
-		return {container:(this.props.mod || "search-box")};
+		this.state = {
+			focused:false
+		}
 	}
 
 	componentWillMount() {
 		super.componentWillMount()
-		this.suggestSearcher = new ESTransport(this.searchkit.host)
+	}
+
+	defineBEMBlocks() {
+		return {container:(this.props.mod || "search-box")};
 	}
 
 	defineAccessor(){
@@ -56,117 +53,39 @@ export class SearchBox extends SearchkitComponent<ISearchBox, any> {
 		this.searchkit.performSearch()
 	}
 
-	processSuggestions(results) {
-		let suggestOptions = _.map(_.get(results.suggest, "suggestions[0].options",[]),option => option)
-		let quickJumpOptions = _.map(_.get(results.suggest, "completion[0].options",[]),option => option)
-
-		return [
-			{
-				sectionName:"Search for",
-				suggestions: suggestOptions
-			},
-			{
-				sectionName:"Images",
-				suggestions:quickJumpOptions
-			}
-		]
-
-	}
-
-	getSuggestionQueryObject(query) {
-		return {
-			size:0,
-			suggest: {
-				text:query,
-				"suggestions":{
-					"phrase": {
-						field:"_all",
-						"real_word_error_likelihood" : 0.95,
-						"max_errors" : 1,
-						"gram_size" : 4,
-						"direct_generator" : [ {
-							"field" : "_all",
-							"suggest_mode" : "always",
-							"min_word_length" : 1
-						} ],
-						"highlight": {
-							"pre_tag": "<em>",
-							"post_tag": "</em>"
-						}
-					}
-				},
-				"completion": {
-					completion: {
-						field:"suggest"
-					}
-				}
-			}
-		}
-	}
-
-	querySuggestions(query, callback) {
-		if (query.length > 0 && this.props.autocomplete) {
-
-			this.suggestSearcher
-				.search(this.getSuggestionQueryObject(query))
-				.then((results:any) => {
-					callback(null, this.processSuggestions(results));
-				})
-		} else {
-			callback(null, [])
-		}
-
-	}
-
-	suggestionRenderer(suggestion, input) {
-		return (<div ref={suggestion.text} dangerouslySetInnerHTML={{__html: suggestion.highlighted || suggestion.text}}></div>)
-	}
-
 	getValue(){
 		return (this.accessor.state.getValue() || "") + ""
 	}
 
 	onChange(e){
-		const value = e.target.value;
-		this.accessor.state = this.accessor.state.setValue(value)
+		const query = e.target.value;
+		this.accessor.state = this.accessor.state.setValue(query)
 		if (this.props.searchOnChange) {
 			_.debounce(()=> {
 				this.searchQuery(this.accessor.state.getValue())
-			}, 300)()
+			}, 200)()
 		}
 	}
 
-	suggestionValue(suggestion) {
-		return suggestion.text;
-	}
-
-	onSuggestionSelected(value){
-
-		this.accessor.state = this.accessor.state.setValue(value.text || value)
-		this.searchkit.search()
+	setFocusState(focused:boolean) {
+		this.setState({focused:focused})
 	}
 
 	render() {
 		let block = this.bemBlocks.container
-		var inputAttributes = {
-			className:block("text"),
-			placeholder:"search",
-			type:"text",
-			ref:"queryField",
-			onInput: this.onChange.bind(this)
-		}
 
 		return (
-			<div className={block()}>
+			<div className={block().state({focused:this.state.focused})}>
         <form onSubmit={this.onSubmit.bind(this)}>
           <div className={block("icon")}></div>
-          <Autosuggest
-						suggestions={this.querySuggestions.bind(this)}
-						suggestionRenderer={this.suggestionRenderer.bind(this)}
-						onSuggestionSelected={this.onSuggestionSelected.bind(this)}
-						suggestionValue={this.suggestionValue}
-						value={this.getValue()}
-						inputAttributes={inputAttributes}/>
+          <input type="text"
+					className={block("text")}
+					placeholder="search"
+					defaultValue={this.getValue()}
+					onFocus={this.setFocusState.bind(this, true)}
+					onBlur={this.setFocusState.bind(this, false)}
+					ref="queryField"
+					onInput={this.onChange.bind(this)}/>
           <input type="submit" value="search" className={block("action")}/>
 					<div className={block("loader").mix("sk-spinning-loader").state({hidden:!this.isLoading()})}></div>
         </form>
