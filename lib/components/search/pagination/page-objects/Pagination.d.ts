bcecff5b7d0573d40326683a1bb0259cbd35d5f0
Ash <ssetem@googlemail.com>
2016-01-07 15:52:04 +0000

    add quick test e2e app

---
+++ lib/components/search/pagination/page-objects/Pagination.d.ts
@@ -0,0 +1,5 @@
+import { Component } from "xenon";
+export default class Pagination extends Component {
+    prevLink: Component;
+    nextLink: Component;
+}
---
+++ lib/components/search/sorting-selector/page-objects/SortingSelector.d.ts
@@ -0,0 +1,3 @@
+import { Component } from "xenon";
+export declare class SortingSelector extends Component {
+}
