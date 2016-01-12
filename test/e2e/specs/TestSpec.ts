be48683d58cdf8ad975aed386c82dadcff89d4f8
Joseph McElroy <phoey1@gmail.com>
2016-01-12 15:37:57 +0000

    e2e of searchbox

--- test/e2e/specs/TestSpec.ts
+++ test/e2e/specs/TestSpec.ts
@@ -1,10 +1,28 @@
-import {Component, field} from "xenon";
+import {Component, field, defaults} from "xenon";
 import Hits from "../../../src/components/search/hits/page-objects/Hits.ts";
+import Searchbox from "../../../src/components/search/search-box/page-objects/SearchBox.ts";
+
+class Hit extends Component {
+
+  @field(Component, {qa:"title"})
+  title:Component
+
+}
+
+@defaults({qa:"hits", itemQA:"hit", itemType:Hit})
+class MovieHits extends Hits {
+
+}
 
 class SearchPage extends Component {
 
-  @field(Hits)
-  hits:Hits
+  @field(MovieHits)
+  hits:MovieHits
+
+  @field(Searchbox)
+  searchbox:Searchbox
+
+
 
 }
 
@@ -17,8 +35,14 @@ describe("example", () => {
 
   it("should show hits", () => {
     expect(this.searchPage.hits.isVisible(20000)).toBe(true);
-    // browser.pause()
     expect(this.searchPage.hits.count()).toBe(10)
+  })
+
+  it("should find matrix", () => {
+    this.searchPage.searchbox.query.type("matrix")
+    expect(this.searchPage.hits.get(0).isVisible()).toBe(true)
+    expect(this.searchPage.hits.get(0).title.getText()).toBe("The Matrix")
+    expect(this.searchPage.hits.count()).toBe(3)
   })
 
 })
