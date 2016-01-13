c9caefe8fb5376044151ac90a1d37013808d53a8
Joseph McElroy <phoey1@gmail.com>
2016-01-13 16:37:34 +0000

    generated

---
+++ lib/src/core/query/query_dsl/compound/FilteredQuery.d.ts
@@ -0,0 +1,7 @@
+export interface FilteredQueryOptions {
+    filter?: any;
+    query?: any;
+}
+export declare function FilteredQuery(filtered: FilteredQueryOptions): {
+    filtered: FilteredQueryOptions;
+};
