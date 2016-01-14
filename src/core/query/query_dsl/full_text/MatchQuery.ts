36cccc8609a1c8705e6bd8afea24537fe1cc30d9
Ash <ssetem@googlemail.com>
2016-01-14 21:07:49 +0000

    add MatchQuery

---
+++ src/core/query/query_dsl/full_text/MatchQuery.ts
@@ -0,0 +1,20 @@
+export interface MatchQueryOptions {
+  operator?:string,
+  zero_terms_query?:string,
+  analyzer?:string,
+  type?:string,
+  cutoff_frequency?:number,
+  max_expansions?:number
+}
+
+export function MatchQuery(field, query, options:MatchQueryOptions={}){
+  if(!query || !field){
+    return
+  }
+
+  return {
+    match:{
+      [field]:_.extend({query}, options)
+    }
+  }
+}
