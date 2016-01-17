f0f22d52f0284a3b5a05c0cbedaa2cc3a6ba6ecb
Siavash <ssetem@googlemail.com>
2016-01-17 14:34:31 +0000

    Update v0.2.0 docs

---
+++ v0.2.0/v0.1.13/lib/build-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+printf "Which tag do you want to build the docs for? "
+read tag
+
+rm -rf $tag
+rm -rf docs
+
+sha=`git rev-list -1 $tag`
+
+git checkout $sha -- docs README.md
+git reset docs
+
+gitbook build
+
+mv _book $tag
+git clean docs  -f
+git checkout HEAD -- README.md
+
+git add $tag
+git commit -m "Update $tag docs"
