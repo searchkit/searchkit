5e923af3726a2890ef65c7eecaf8f6accea41a92
Joseph McElroy <phoey1@gmail.com>
2016-01-17 13:34:38 +0000

    No hits rendering spec

--- src/components/search/hits/test/NoHitsSpec.tsx
+++ src/components/search/hits/test/NoHitsSpec.tsx
@@ -5,7 +5,7 @@ import {SearchkitManager} from "../../../../core";
 import * as _ from "lodash";
 import * as sinon from "sinon";
 
-xdescribe("NoHits component", () => {
+fdescribe("NoHits component", () => {
 
   beforeEach(() => {
 
@@ -13,39 +13,61 @@ xdescribe("NoHits component", () => {
 
     this.createWrapper = () => {
       this.wrapper = mount(
-        <NoHits searchkit={this.searchkit} translations={{"nohits.no_results":"no movies"}}/>
+        <NoHits searchkit={this.searchkit} translations={{"NoHits.NoResultsFound":"no movies"}}/>
       )
     }
 
   });
 
 
-  it('renders correctly', () => {
-    this.createWrapper()
-  //   this.searchkit.initialLoading = true
-  //   this.wrapper.update()
-  //   expect(this.wrapper.find('[data-qa="no-hits"]').length).toBe(0)
-  //   this.searchkit.initialLoading = false
-  //   this.searchkit.setResults({
-  //     hits:{
-  //       hits:[1,2,3],
-  //       total:3
-  //     }
-  //   })
-  //   this.wrapper.update()
-  //   expect(this.wrapper.find('[data-qa="no-hits"]').length).toBe(0)
-  //
-  //   this.searchkit.setResults({
-  //     hits:{
-  //       hits:[],
-  //       total:0
-  //     }
-  //   })
-  //   this.wrapper.update()
-  //
-  //   expect(this.wrapper.find('[data-qa="no-hits"]').length).toBe(1)
-  //   expect(this.wrapper.find('[data-qa="no-hits"]').text())
-  //     .toBe("no movies")
-  //
+  describe('renders correctly', () => {
+
+    beforeEach(() => {
+      this.createWrapper()
+      this.hasRendered = () => {
+        return this.wrapper.find(".no-hits").length == 1
+      }
+    })
+
+    it("doesn't render on initial load", () => {
+      this.searchkit.initialLoading = true
+      this.wrapper.update()
+      expect(this.hasRendered()).toBeFalsy()
+    })
+
+    it("doesn't render on hits", () => {
+      this.searchkit.initialLoading = false
+      this.searchkit.setResults({
+        hits:{
+          hits:[1,2,3],
+          total:3
+        }
+      })
+      this.wrapper.update()
+      expect(this.hasRendered()).toBeFalsy()
+
+    })
+
+    it("doesn't render on loading", () => {
+      this.searchkit.isLoading = () => { return true }
+      this.wrapper.update()
+      expect(this.hasRendered()).toBeFalsy()
+    })
+
+    it("renders on no hits", () => {
+
+      this.searchkit.setResults({
+        hits:{
+          hits:[],
+          total:0
+        }
+      })
+      this.wrapper.update()
+      expect(this.hasRendered()).toBeTruthy()
+      expect(this.wrapper.find('.no-hits__info').text())
+        .toBe("no movies")
+
+    })
+
   });
 });
