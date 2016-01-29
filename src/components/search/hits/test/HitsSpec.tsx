d72507d1a9ee43051872a45d3f84c0821daa5d9f
Ash <ssetem@googlemail.com>
2016-01-29 18:31:27 +0000

    make hits higher order for hit item

--- src/components/search/hits/test/HitsSpec.tsx
+++ src/components/search/hits/test/HitsSpec.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import {mount} from "enzyme";
 import {Hits} from "../src/Hits";
 import {SearchkitManager} from "../../../../core";
-
+import {
+  fastClick, hasClass, jsxToHTML, printPrettyHtml
+} from "../../../__test__/TestHelpers"
 import * as sinon from "sinon";
 
 describe("Hits component", () => {
@@ -66,6 +68,27 @@ describe("Hits component", () => {
       this.wrapper.update()
       expect(this.hasRendered()).toBeFalsy()
       expect(this.wrapper.find(".hits__initial-loading").length).toBe(0)
+    })
+
+    it("custom higher order component", ()=> {
+      this.searchkit.setResults({
+        hits:{
+          hits:[{_id:1, title:1},{_id:2,title:2}],
+          total:2
+        }
+      })
+      let hit = (props)=> {
+        return <div className={props.bemBlocks.item()}>{props.result.title}</div>
+      }
+      let wrapper = mount(
+        <Hits searchkit={this.searchkit} component={hit} hitsPerPage={10}/>
+      )
+      expect(wrapper.html()).toEqual(jsxToHTML(
+        <div data-qa="hits" className="hits">
+          <div className="hits-hit">1</div>
+          <div className="hits-hit">2</div>
+        </div>
+      ))
     })
   })
 
