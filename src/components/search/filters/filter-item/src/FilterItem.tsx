9177d43c04bf22cf8e180c11de847fcdd654f820
Joseph McElroy <phoey1@gmail.com>
2016-02-02 13:30:49 +0000

    Add FilterCheckboxItemComponent Item

--- src/components/search/filters/filter-item/src/FilterItem.tsx
+++ src/components/search/filters/filter-item/src/FilterItem.tsx
@@ -11,7 +11,6 @@ import {
 export interface FilterItemComponentProps {
 	bemBlocks:{container:any, option:any},
 	toggleFilter:Function,
-	resetFilters:Function,
 	translate:Function,
   selected:Boolean,
   label:string,
@@ -20,18 +19,38 @@ export interface FilterItemComponentProps {
 
 export class FilterItemComponent extends React.Component<FilterItemComponentProps, any>{
 	render(){
-		const {bemBlocks, toggleFilter, translate, resetFilters, selected, label, count} = this.props
+		const {bemBlocks, toggleFilter, translate, selected, label, count} = this.props
     const className = bemBlocks.option()
 											.state({selected})
 											.mix(bemBlocks.container("item"))
 
 		return (
       <FastClick handler={toggleFilter} key={label}>
-				<div className={className}>
-					<div className={bemBlocks.option("text")}>{label}</div>
-					<div className={bemBlocks.option("count")}>{count}</div>
+				<div className={className} data-qa="option">
+					<div data-qa="label" className={bemBlocks.option("text")}>{label}</div>
+					<div data-qa="count" className={bemBlocks.option("count")}>{count}</div>
 				</div>
 			</FastClick>
+		)
+	}
+}
+
+export class FilterCheckboxItemComponent extends React.Component<FilterItemComponentProps, any>{
+	render(){
+    const {bemBlocks, toggleFilter, translate, selected, label, count} = this.props
+    const className = bemBlocks.option()
+											.state({selected})
+											.mix(bemBlocks.container("item"))
+    const block = bemBlocks.option
+
+		return (
+      <FastClick handler={toggleFilter}>
+        <div className={className} data-qa="option">
+          <div data-qa="checkbox" className={block("checkbox").state({ selected }) }></div>
+          <div data-qa="label" className={block("text") }>{label}</div>
+          <div data-qa="count" className={block("count") }>{count}</div>
+        </div>
+      </FastClick>
 		)
 	}
 }
