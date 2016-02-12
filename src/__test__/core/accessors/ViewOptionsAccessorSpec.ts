6444e058245a48436cbc7e70c0c89b7f767e8f52
Joseph McElroy <phoey1@gmail.com>
2016-02-12 18:55:25 +0000

    spy performSearch

--- src/__test__/core/accessors/ViewOptionsAccessorSpec.ts
+++ src/__test__/core/accessors/ViewOptionsAccessorSpec.ts
@@ -4,22 +4,31 @@ import {
   ImmutableQuery
 } from "../../../"
 
+import * as sinon from "sinon";
+
+
 describe("ViewOptionsAccessor", ()=> {
 
   beforeEach(()=> {
     this.searchkit = SearchkitManager.mock()
     this.accessor = new ViewOptionsAccessor("view",{})
     this.searchkit.addAccessor(this.accessor)
+    this.spy = sinon.spy(this.searchkit, "performSearch")
+
   })
 
   it("should set view", () => {
     this.accessor.setView({key:"grid", defaultOption:false})
     expect(this.accessor.state.getValue()).toBe("grid")
+    expect(this.spy.calledOnce).toEqual(true)
+    expect(this.spy.calledWith(false,false)).toEqual(true)
   })
 
   it("should set view - default option", () => {
     this.accessor.setView({key:"grid", defaultOption:true})
     expect(this.accessor.state.getValue()).toBe(null)
+    expect(this.spy.calledOnce).toEqual(true)
+    expect(this.spy.calledWith(false,false)).toEqual(true)
   })
 
 
