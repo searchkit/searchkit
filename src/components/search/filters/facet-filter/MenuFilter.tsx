35c0a5f0396dc47ee078a247626a7ae44ab6965a
Ash <ssetem@googlemail.com>
2016-03-03 20:19:42 +0000

    WIP facet filters

---
+++ src/components/search/filters/facet-filter/MenuFilter.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+import { ItemList } from '../../../../'
+import { FacetFilter} from "./FacetFilter"
+
+const defaults = require("lodash/defaults")
+const map = require("lodash/map")
+const concat = require("lodash/concat")
+
+import {FacetFilterProps, FacetFilterPropTypes} from "./FacetFilterProps"
+
+export class MenuFilter2 extends FacetFilter<FacetFilterProps> {
+
+  static propTypes = defaults({
+  },FacetFilterPropTypes.propTypes)
+
+  static defaultProps = defaults({
+    listComponent: ItemList,
+    operator:"OR"
+  }, FacetFilter.defaultProps)
+
+  static AllItem = {
+    key:"All"
+  }
+
+  toggleFilter(option) {
+    if (option === MenuFilter2.AllItem.key){
+      this.accessor.state = this.accessor.state.clear()
+    } else {
+      this.accessor.state = this.accessor.state.toggle(option);
+    }
+    this.searchkit.performSearch()
+  }
+
+  setFilters(options){
+    this.toggleFilter(options[0])
+  }
+
+  getSelectedItems() {
+    return [this.accessor.state.getValue()[0] || MenuFilter2.AllItem.key]
+  }
+
+  getItems(){
+    return concat(this.accessor.getBuckets(), MenuFilter2.AllItem)
+  }
+
+
+}
--- test/e2e/server/apps/ui-app/index.tsx
+++ test/e2e/server/apps/ui-app/index.tsx
@@ -3,7 +3,8 @@ const {
   SearchBox, Hits, RefinementListFilter, Pagination,
   HierarchicalMenuFilter, HitsStats, SortingSelector, NoHits,
   SelectedFilters, ResetFilters, RangeFilter, NumericRefinementListFilter,
-  Panel, TagCloud, Toggle, Select, Tabs, ItemList, CheckboxItemList
+  Panel, TagCloud, Toggle, Select, Tabs, ItemList, CheckboxItemList, CheckboxFilter,
+  RefinementListFilter2, MenuFilter2
 } = require("../../../../../src")
 
 const host = "http://demo.searchkit.co/api/movies"
@@ -42,12 +43,20 @@ class App extends React.Component<any, any> {
               <Panel title='My Panel' collapsable={true}>
                 <p>panel contents</p>
               </Panel>
-              <MockList title="Tag Cloud" listComponent={TagCloud}/>
-              <MockList title="Toggle" listComponent={Toggle}/>
-              <MockList title="Select" listComponent={Select}/>
-              <MockList title="Tabs" listComponent={Tabs}/>
-              <MockList title="Item List" listComponent={ItemList}/>
-              <MockList title="Checkbox List" listComponent={CheckboxItemList}/>
+                <MockList title="Tag Cloud" listComponent={TagCloud}/>
+                <MockList title="Toggle" listComponent={Toggle}/>
+                <MockList title="Select" listComponent={Select}/>
+                <MockList title="Tabs" listComponent={Tabs}/>
+                <MockList title="Item List" listComponent={ItemList}/>
+                <MockList title="Checkbox List" listComponent={CheckboxItemList}/>
+                <CheckboxFilter id="rating" title="Rating" field="rated.raw" value="R" label="Rated 'R'"/>
+                <RefinementListFilter2
+                  translations={{ "facets.view_more": "View more writers" }}
+                  id="writers" title="Writers" field="writers.raw" size={10}
+                  listComponent={CheckboxItemList}
+                />
+                <MenuFilter2 field="type.raw" title="Movie Type" id="move_type" showCount={true} listComponent={Tabs}/>
+
             </div>
 
             <div className="sk-layout__results sk-results-list">
