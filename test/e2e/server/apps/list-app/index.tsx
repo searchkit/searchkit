a1fb7a560bb3dedba18546a5afc12eebfb488e6f
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-03-13 00:20:36 +0100

    add range-app demo showcasing the rangeComponents

---
+++ test/e2e/server/apps/list-app/index.tsx
@@ -0,0 +1,95 @@
+const {
+  SearchkitManager,SearchkitProvider,
+  SearchBox, Hits, RefinementListFilter, Pagination,
+  MenuFilter, HitsStats, SortingSelector, NoHits,
+  ItemList, CheckboxItemList, ItemHistogramList, 
+  Tabs, TagCloud, Toggle, Select
+} = require("../../../../../src")
+const host = "http://demo.searchkit.co/api/movies"
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+const searchkit = new SearchkitManager(host)
+
+const _ = require("lodash")
+
+require("../../../../../theming/theme.scss")
+require("./customisations.scss")
+
+const MovieHitsGridItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  const source:any = _.extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <a href={url} target="_blank">
+        <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+const MovieHitsListItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  const source:any = _.extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <img data-qa="poster" src={result._source.poster}/>
+      </div>
+      <div className={bemBlocks.item("details")}>
+        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
+        <h3 className={bemBlocks.item("subtitle")}>Released in {source.year}, rated {source.imdbRating}/10</h3>
+        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:source.plot}}></div>
+      </div>
+    </div>
+  )
+}
+
+class App extends React.Component<any, any> {
+  render(){
+    return (
+      <SearchkitProvider searchkit={searchkit}>
+        <div className="sk-layout list-app">
+
+          <div className="sk-layout__top-bar sk-top-bar">
+            <div className="sk-top-bar__content">
+              <div className="my-logo">Filter list components</div>
+              <SearchBox autofocus={true} searchOnChange={true} prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+            </div>
+          </div>
+
+          <div className="sk-layout__body">
+
+            <div className="sk-layout__filters">
+              <div className="sk-layout__filters-row">
+                <MenuFilter field={"type.raw"} title="ItemList" id="item-list" listComponent={ItemList} />
+                <MenuFilter field={"type.raw"} title="CheckboxItemList" id="checkbox-item-list" listComponent={CheckboxItemList} />
+                <MenuFilter field={"type.raw"} title="ItemHistogramList" id="histogram-list" listComponent={ItemHistogramList} />
+                <MenuFilter field={"type.raw"} title="TagCloud" id="tag-cloud" listComponent={TagCloud} />
+                <MenuFilter field={"type.raw"} title="Toggle" id="toggle" listComponent={Toggle} />
+                <MenuFilter field={"type.raw"} title="Tabs" id="tabs" listComponent={Tabs} />
+                <MenuFilter field={"type.raw"} title="Select" id="select" listComponent={Select} />
+              
+              </div>
+            </div>
+
+            <div className="sk-layout__results sk-results-list">
+              <Hits
+                  hitsPerPage={12} highlightFields={["title","plot"]}
+                  sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}
+                  mod="sk-hits-grid" itemComponent={MovieHitsGridItem} scrollTo="body"
+              />
+              <NoHits suggestionsField={"title"}/>
+              <Pagination showNumbers={true}/>
+            </div>
+          </div>
+        </div>
+      </SearchkitProvider>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
