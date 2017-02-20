508faad57743b6b63a111de9fb7ef4158cd7e232
Joseph McElroy <phoey1@gmail.com>
2017-02-20 09:36:38 +0000

    upgrade first commit

--- src/core/history.ts
+++ src/core/history.ts
@@ -1,13 +1,24 @@
 import { createHistory, useQueries, History, HistoryQueries } from 'history'
 const qs = require("qs")
+const qsUtils = require("qs/lib/utils")
+
+export const encodeObjUrl = (obj) => {
+  return qs.stringify(obj, { encode: true, encoder: (str, isPrefix) => {
+    return isPrefix ? str : qsUtils.encode(str)
+  }})
+}
+
+export const decodeObjString = (str) => {
+  return qs.parse(str)
+}
 
 export const createHistoryInstance = function(){
   return useQueries(createHistory)({
     stringifyQuery(ob){
-      return qs.stringify(ob, {encode:true})
+      return encodeObjUrl(ob)
     },
     parseQueryString(str){
-      return qs.parse(str)
+      return decodeObjString(str)
     }
   })
 }
