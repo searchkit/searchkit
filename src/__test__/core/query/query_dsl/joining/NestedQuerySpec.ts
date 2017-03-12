adaf4f3d60a77e5694fd6ab97caa954bf6cbafb5
Rafal Lyczkowski <rafal@mybusinessmatches.com>
2017-03-12 16:38:43 +0100

    Fixes elasticsearch 5.1 parsing exception: [nested] query does not support [filter] #413

--- src/__test__/core/query/query_dsl/joining/NestedQuerySpec.ts
+++ src/__test__/core/query/query_dsl/joining/NestedQuerySpec.ts
@@ -6,14 +6,14 @@ it("NestedQuery", ()=> {
   expect(NestedQuery("taxonomy", "somequery")).toEqual({
     nested:{
       path:"taxonomy",
-      filter:"somequery"
+      query:"somequery"
     }
   })
 
   expect(NestedQuery("taxonomy", "somequery", {score_mode:"sum", invalid:"foo", inner_hits:{}})).toEqual({
     nested:{
       path:"taxonomy",
-      filter:"somequery",
+      query:"somequery",
       score_mode:"sum",
       inner_hits:{}
     }
