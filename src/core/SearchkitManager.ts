c6d12a90415f8e0a9f59894ada1e344b30b574ee
Ash <ssetem@googlemail.com>
2017-03-29 13:20:44 +0100

    History updates (#430)
    * update history 4.6.1, pull up query deserial, remove reference counting code
    
    * Allow history object to be customised
    
    * Fix SearchkitManager tests
    
    * Allow location function to be passed in and not depend on window object
    
    * Use encodeObjUrl method

--- src/core/SearchkitManager.ts
+++ src/core/SearchkitManager.ts
@@ -1,11 +1,11 @@
 import {ImmutableQuery} from "./query";
 import {Accessor, BaseQueryAccessor, AnonymousAccessor} from "./accessors"
 import {AccessorManager} from "./AccessorManager"
-import {createHistoryInstance} from "./history";
 import {ESTransport, AxiosESTransport, MockESTransport} from "./transport"
 import {SearchRequest} from "./SearchRequest"
 import {Utils, EventEmitter} from "./support"
 import {VERSION} from "./SearchkitVersion"
+import {createHistoryInstance, encodeObjUrl, decodeObjString} from "./history"
 
 import {defaults} from "lodash"
 import {constant} from "lodash"
@@ -21,6 +21,8 @@ import {after} from "lodash"
 
 export interface SearchkitOptions {
   useHistory?:boolean,
+  createHistory?:Function,
+  getLocation?:Function,
   searchOnLoad?:boolean,
   httpHeaders?:Object,
   basicAuth?:string,
@@ -65,7 +67,9 @@ export class SearchkitManager {
     this.options = defaults(options, {
       useHistory:true,
       httpHeaders:{},
-      searchOnLoad:true
+      searchOnLoad:true,
+      createHistory:createHistoryInstance,
+      getLocation:()=> window.location
     })
     this.host = host
 
@@ -91,11 +95,10 @@ export class SearchkitManager {
     this.initialLoading = true
     if(this.options.useHistory) {
       this.unlistenHistory()
-      this.history = createHistoryInstance()
+      this.history = this.options.createHistory()
       this.listenToHistory()
-    } else {
-      this.runInitialSearch()
     }
+    this.runInitialSearch()
   }
   addAccessor(accessor){
     accessor.setSearchkitManager(this)
@@ -136,25 +139,24 @@ export class SearchkitManager {
     }
   }
   listenToHistory(){
-    let callsBeforeListen = (this.options.searchOnLoad) ? 1: 2
-
-    this._unlistenHistory = this.history.listen(after(callsBeforeListen,(location)=>{
-      //action is POP when the browser modified
-      if(location.action === "POP") {
-        this.registrationCompleted.then(()=>{
-          this.searchFromUrlQuery(location.query)
-        }).catch((e)=> {
-          console.error(e.stack)
-        })
+    this._unlistenHistory = this.history.listen((location, action)=>{
+      if(action === "POP") {
+        this._searchWhenCompleted(location)
       }
-    }))
+    })
+  }
+
+  _searchWhenCompleted(location){
+    this.registrationCompleted.then(()=> {
+      this.searchFromUrlQuery(decodeObjString(location.search.replace(/^\?/, "")))
+    }).catch((e)=> {
+      console.error(e.stack)
+    })
   }
 
   runInitialSearch(){
     if(this.options.searchOnLoad) {
-      this.registrationCompleted.then(()=> {
-        this._search()
-      })
+      this._searchWhenCompleted(this.options.getLocation())
     }
   }
 
@@ -171,14 +173,15 @@ export class SearchkitManager {
     if(this.options.useHistory){
       const historyMethod = (replaceState) ?
         this.history.replace : this.history.push
-      historyMethod({pathname: window.location.pathname, query:this.state})
+
+      let url = this.options.getLocation().pathname + "?" + encodeObjUrl(this.state)
+      historyMethod.call(this.history, url)
     }
   }
 
   buildSearchUrl(extraParams = {}){
-    const params = defaults(extraParams, this.state || this.accessors.getState())
-    const queryString = qs.stringify(params, { encode: true })
-    return window.location.pathname + '?' + queryString
+    const params = defaults(extraParams, this.state || this.accessors.getState())    
+    return this.options.getLocation().pathname + '?' + encodeObjUrl(params)
   }
 
   reloadSearch(){
