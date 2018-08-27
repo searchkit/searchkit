f986a95e228c6bf61f2f4855ba7679dc55fff3c9
Joseph McElroy <phoey1@gmail.com>
2018-08-27 09:23:38 +0100

    update versioning

--- packages/searchkit/scripts/writeVersion.js
+++ packages/searchkit/scripts/writeVersion.js
@@ -1,7 +1,12 @@
-let version = require("../package.json").version
-let fs = require("fs")
-let path = require("path")
-fs.writeFileSync(
-    path.join(__dirname, "../", "src/core", "SearchkitVersion.ts"),
-    `export const VERSION = '${version}'`
-)
+if (process.env.BRANCH) {
+
+  const version = process.env.BRANCH.replace('v', '');
+
+  let fs = require("fs")
+  let path = require("path")
+  fs.writeFileSync(
+      path.join(__dirname, "../", "src/core", "SearchkitVersion.ts"),
+      `export const VERSION = '${version}'`
+  )
+
+}
