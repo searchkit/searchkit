9c1651b1fe41b2d1bdd05285c110b8ebffe8cbd9
laurent.corron <laurent.corron@acsone.eu>
2019-12-13 16:24:19 +0100

    [FIX] fix test after url encoding change

--- packages/searchkit/src/__test__/core/history/HistorySpec.ts
+++ packages/searchkit/src/__test__/core/history/HistorySpec.ts
@@ -5,7 +5,7 @@ describe("History", () => {
   it("encode / decode searchkit obj", () => {
     const obj = {q: "test", categories: [["movie"],["Crime"]], actors: ["a", 'b-c', 'd & e', 'f=g'], writers: ['d'] }
     const str = history.encodeObjUrl(obj)
-    expect(str).toEqual('q=test&categories[0][0]=movie&categories[1][0]=Crime&actors[0]=a&actors[1]=b-c&actors[2]=d%20%26%20e&actors[3]=f%3Dg&writers[0]=d')
+    expect(str).toEqual('q=test&categories%255B0%255D%255B0%255D=movie&categories%255B1%255D%255B0%255D=Crime&actors%255B0%255D=a&actors%255B1%255D=b-c&actors%255B2%255D=d%2520%2526%2520e&actors%255B3%255D=f%253Dg&writers%255B0%255D=d')
     expect(history.decodeObjString(str)).toEqual(obj)
   })
 
