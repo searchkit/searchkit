609baf0a74951c78dc3585b974e601cadd83785e
Joseph McElroy <phoey1@gmail.com>
2019-12-28 19:39:10 +0000

    fix build steps

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const isProdRelease = process.env.BRANCH && process.env.BRANCH === "master"
 const isPublishCommit = process.env.PUBLISH_COMMIT && process.env.PUBLISH_COMMIT === "Publish"
-// const spawn = require('cross-spawn');
-// const exec = require('child_process').execSync;
+const spawn = require('cross-spawn');
+const exec = require('child_process').execSync;
 
 const spawnWithErrorHandling = (...args) => {
   const results = spawn.sync(...args);
@@ -27,16 +27,16 @@ if (!isProdRelease) { // pre-release
 }
 
 if (isPublishCommit) {
-  console.log("publishing", tasks)
+
+  // Publish packages to npm registry
+  spawnWithErrorHandling('npm', tasks, { stdio: 'inherit' });
+
+  console.log('Pushing commit...');
+  exec('git checkout staging');
+  exec('git add .');
+  exec(`git commit -m 'Publish'`);
+  exec(`git push origin ${process.env.BRANCH}`);
+
+  console.log('Done!');
+
 }
-
-// Publish packages to npm registry
-// spawnWithErrorHandling('npm', tasks, { stdio: 'inherit' });
-
-// console.log('Pushing commit...');
-// exec('git checkout staging');
-// exec('git add .');
-// exec(`git commit -m publish packages`);
-// exec(`git push origin ${process.env.BRANCH}`);
-
-// console.log('Done!');
