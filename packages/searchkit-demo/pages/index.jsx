539807794e704d94f4a962222c5aa92959f9852d
Joseph McElroy <phoey1@gmail.com>
2020-11-08 20:39:37 +0000

    demo: remove unused code

--- packages/searchkit-demo/pages/index.jsx
+++ packages/searchkit-demo/pages/index.jsx
@@ -2,8 +2,4 @@ import { withSearchkit } from '@searchkit/client'
 import withApollo from '../hocs/withApollo'
 import Search from '../components/index'
 
-const Index = (props) => {
-  return <Search />
-}
-
 export default withApollo(withSearchkit(Search))
