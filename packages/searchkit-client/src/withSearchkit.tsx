9d088329a0b3028fc18d7de8fd3a89deade0bb8e
Joseph McElroy <phoey1@gmail.com>
2020-11-08 20:38:03 +0000

    update withSearchkit hoc

--- packages/searchkit-client/src/withSearchkit.tsx
+++ packages/searchkit-client/src/withSearchkit.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { SearchkitProvider, SearchkitClient } from './searchkit'
 
 const defaultSearchkitClient = () => new SearchkitClient()
+let _client = null
 
 export default (Page, createSearchkitClient = defaultSearchkitClient) => {
-  const client = createSearchkitClient()
+  const getClient = () => {
+    if (typeof window === 'undefined') {
+      return createSearchkitClient()
+    }
+    if (!_client) {
+      _client = createSearchkitClient()
+    }
+    return _client
+  }
+
   return () => (
-    <SearchkitProvider client={client}>
+    <SearchkitProvider client={getClient()}>
       <Page />
     </SearchkitProvider>
   )
