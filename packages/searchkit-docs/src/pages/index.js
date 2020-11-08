dfdab3627bfcf3cddd08285252ba18a7b3cac1e8
Joseph McElroy <phoey1@gmail.com>
2020-11-08 13:11:43 +0000

    documentation updates

--- packages/searchkit-docs/src/pages/index.js
+++ packages/searchkit-docs/src/pages/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+[object Object]
+ */
 import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
