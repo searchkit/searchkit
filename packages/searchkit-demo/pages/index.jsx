129b6acd2c83536f12e30e9d766e6536394df535
Joseph McElroy <phoey1@gmail.com>
2020-11-11 20:24:13 +0000

    update documentation

--- packages/searchkit-demo/pages/index.jsx
+++ packages/searchkit-demo/pages/index.jsx
@@ -1,6 +1,7 @@
 import { withSearchkit } from '@searchkit/client'
+import dynamic from 'next/dynamic'
 import withApollo from '../hocs/withApollo'
-import Search from '../components/index'
 
+const Search = dynamic(() => import('../components/index'), { ssr: false })
 
 export default withApollo(withSearchkit(Search))
