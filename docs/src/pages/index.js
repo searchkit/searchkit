4923c3e55e34388ee349d2246b0f14f7dc7519b2
Joseph McElroy <phoey1@gmail.com>
2020-11-13 17:02:12 +0000

    update docs site

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -9,7 +9,6 @@
 import React, { useEffect, useState } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import GithubLogo from './components/GithubLogo';
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
 
@@ -24,13 +23,13 @@ const Navigation = () => (
           <div className="md:col-span-9 items-center flex justify-between md:justify-end  space-x-6 h-16">
             <div className="flex justify-between md:justify-end items-center flex-1 md:space-x-2">
               <div>
-                <a href="/docs" className="rounded-md py-2 px-3 inline-flex items-center leading-5 font-medium text-gray-100 betterhover:hover:bg-gray-100 hover:text-gray-900 hover:bg-gray-200 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition duration-150 ease-in-out">
+                <a href="/docs" className="rounded-md py-2 px-3 inline-flex items-center leading-5 font-medium text-gray-100 betterhover:hover:bg-gray-100 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition duration-150 ease-in-out">
                   Docs
                 </a>
               </div>
               <div>
-                <a href="/github" className="rounded-md py-2 px-3 inline-flex items-center leading-5 font-medium text-gray-100 betterhover:hover:bg-gray-100 hover:text-gray-900 hover:bg-gray-200 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition duration-150 ease-in-out">
-                Github <GithubLogo />
+                <a href="https://github.com/searchkit/searchkit" className="rounded-md py-2 px-3 inline-flex items-center leading-5 font-medium text-gray-100 betterhover:hover:bg-gray-900 hover:text-gray-900 hover:bg-gray-800 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition duration-150 ease-in-out">
+                  <img src={useBaseUrl('img/github.png')} style={{ height: "28px"}} />
                 </a></div>
             </div>
           </div>
@@ -40,7 +39,7 @@ const Navigation = () => (
   </div>
 )
 
-const HeroImage = ({ src='img/m/search.jpeg', width="490px", height="400px" }) => {
+const HeroImage = ({ src='img/m/search.jpeg', width="530px", height="400px" }) => {
 
   return (
     <div className="rounded-lg overflow-hidden shadow-xl inline-block border-black border mx-auto lg:mx-0" style={{
@@ -56,7 +55,7 @@ const HeroImage = ({ src='img/m/search.jpeg', width="490px", height="400px" }) =
 const Hero = () => (
   <div className="relative bg-gray-900 overflow-hidden">
     <div className="py-24 mx-auto container px-4 sm:mt-8 relative">
-      <div className="grid grid-cols-12 gap-4">
+      <div className="grid grid-cols-12 gap-12">
 
         <div className="col-span-12 lg:col-span-5">
           <div className="text-center lg:text-left md:max-w-2xl md:mx-auto">
@@ -68,11 +67,11 @@ const Hero = () => (
               <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:border-gray-500 focus:shadow-outline-white transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10" href="/docs/overview">Get Started</a>
             </div>
             <div className="rounded-md shadow ml-3">
-              <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100 bg-gray-900 hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 border-gray-900" href="/docs/overview">View Demo</a>
+              <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100 bg-gray-900 hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 border-gray-900" href="https://demo.searchkit.co">View Demo</a>
             </div>
           </div>
         </div>
-        <div className="hidden lg:block lg:col-span-7 mb-5 lg:mb-0">
+        <div className="hidden lg:block lg:col-span-7 md:ml-8 mb-5 lg:mb-0">
           <HeroImage />
         </div>
       </div>
@@ -199,20 +198,29 @@ const code2 = `
             <div className="col-span-12 lg:col-span-6 overflow-hidden flex text-sm pb-60 transform transition-opacity duration-300 lg:mr-10">
                 <SyntaxHighlight code={code} className="transform transition-opacity duration-300 w-full" style={{opacity: activeView === "api" ? 0 : 1 }} />
                 <SyntaxHighlight code={code2} language="graphql" className="transform transition-opacity absolute duration-300 w-full" style={{opacity: activeView === "code" ? 0 : 1 }} />
-
             </div>
             <div className="lg:col-span-6 col-span-12 lg:pt-0 pt-6">
               <div className="text-center lg:text-left">
                 <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-3xl lg:text-3xl xl:text-3xl">Powered by Apollo GraphQL</h2>
-                <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Searchkit uses Apollo & GraphQL to </p>
+                <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Searchkit GraphQL resolvers saves building a search focused API with common use cases such as facet filtering, pagination and querying.  
+
+                </p>
                 <div className="mx-auto mt-8 text-base text-center">
                   <SyntaxHighlight code={`yarn add @searchkit/apollo-resolvers`} language="shell" />
                 </div>
+                <div className="mt-5 mx-auto sm:flex justify-left md:mt-8">
+                  <div className="rounded-md shadow">
+                    <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:border-gray-500 focus:shadow-outline-white transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10" href="/docs/quick-start/api-setup">Start Tutorial</a>
+                  </div>
+                  <div className="rounded-md shadow ml-3">
+                    <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-900 bg-gray-000 hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 border-gray-900" href="https://demo.searchkit.co">View Demo</a>
+                  </div>
+              </div>
               </div>
               <div className="flex flex-col justify-between mt-12 sm:flex-row">
                 <div className="flex flex-col justify-between w-4/5">
                   <div className="flex flex-col justify-between">
-                    <h3 className="text-xl font-bold mt-1">Search API in 60 seconds</h3>
+                    <h3 className="text-xl font-bold mt-1">API designed for Search UI</h3>
                     <p className="mt-1">With Apollo, Elasticsearch and Searchkit's resolvers, its extremely quick to build a capable dev friendly search API.</p>
                   </div>
                   <div className="flex flex-col justify-between mt-20">
@@ -222,12 +230,12 @@ const code2 = `
                 </div>
                 <div className="flex flex-col justify-between w-4/5 sm:ml-10">
                   <div className="flex flex-col justify-between mt-20 sm:mt-0">
-                    <h3 className="text-xl font-bold mt-1">Integrated with Apollo</h3>
-                    <p className="mt-1">Leverages Apollo</p>
+                    <h3 className="text-xl font-bold mt-1">Client Friendly</h3>
+                    <p className="mt-1">All the heavy lifting is done by the API, making it easy to integrate on your React website or Swift iOS App.</p>
                   </div>
                   <div className="flex flex-col justify-between mt-20">
-                    <h3 className="text-xl font-bold mt-1">Easily build your own Facets</h3>
-                    <p className="mt-1">kjvhdkjvhvjkhdjfvk jhkjh kjvhdkjvhvjkhdjfvk jhkjh kjvhdkjvhvjkhdjfvk jhkjh</p>
+                    <h3 className="text-xl font-bold mt-1">Flexible Customisation</h3>
+                    <p className="mt-1">Easily extend and add your own facets. Write your own resolvers to provide information not just from elasticsearch.</p>
                   </div>
                 </div>
               </div>
@@ -242,9 +250,9 @@ const code2 = `
 const FrontendIntro = () => {
   return (
     <div className="text-lg border-t border-gray-400 bg-gray-900">
-      <div className="py-24 mx-auto container px-4 sm:mt-8 relative">
-        <div className="md:col-span-6 col-span-12 md:pt-0 pt-6">
-          <video playsInline autoPlay muted loop src={useBaseUrl("movs/search-demo.mov")} />
+      <div className="py-24 mx-auto container px-4 md:mt-8 relative">
+        <div className="md:col-span-6 col-span-12 md:pt-0 md:pt-6 pt-4">
+          <video className="mx-auto" playsInline autoPlay muted loop src={useBaseUrl("movs/search-demo.mov")} />
         </div>
         <div className="md:col-span-6 col-span-12 pt-20">
           <div className="text-center">
