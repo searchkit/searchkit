c8162241e332716fc4fcef81559d1d465710f330
Joseph McElroy <phoey1@gmail.com>
2020-11-17 21:55:11 +0000

    AWS lambda example (#771)
    * Remove console log for query
    
    * switch to using ts-jest
    
    * fix eslint errors
    
    * add searchkit/client unit tests
    
    * add documentation on customisation
    
    * update licence
    
    * Add chat button
    
    * Add aws lambda example

---
+++ examples/aws-lambda/handler.js
@@ -0,0 +1,47 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const {
+  MultiMatchQuery,
+  SearchkitResolvers,
+  SearchkitSchema,
+  RefinementSelectFacet
+} = require('@searchkit/apollo-resolvers')
+
+const searchkitConfig = {
+  host: "https://user:pass@6773f6bc.qb0x.com:32359",
+  index: 'movies',
+  hits: {
+    fields: ['title']
+  },
+  query: new MultiMatchQuery({ fields: ['title'] }),
+  facets: [
+    new RefinementSelectFacet({ field: 'type.raw', id: 'type', label: 'Type' })
+  ]
+}
+
+const typeDefs = [
+  gql`
+    type Query {
+      root: String
+    }
+
+    type Mutation {
+      root: String
+    }
+
+    type HitFields {
+      title: String
+    }
+  `,
+  SearchkitSchema
+]
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    ...SearchkitResolvers(searchkitConfig)
+  },
+  playground: true,
+  introspection: true,
+});
+ 
+exports.graphqlHandler = server.createHandler();
