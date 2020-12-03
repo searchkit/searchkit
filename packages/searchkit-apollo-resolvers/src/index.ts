ed91f46d7e50a11fa8684c0e338ff5d6d9e3b85d
joemcelroy <phoey1@gmail.com>
2020-12-03 20:01:55 +0000

    feat(#788) add custom query class

--- packages/searchkit-apollo-resolvers/src/index.ts
+++ packages/searchkit-apollo-resolvers/src/index.ts
@@ -1,4 +1,4 @@
 export * from './resolvers'
-export { default as MultiMatchQuery } from './query/MultiMatchQuery'
+export * from './query'
 export * from './facets'
 export { default as SearchkitSchema } from './schema'
