ea924b55c40efd8848e44d77e922fa529d37e3fb
Dennis Voltz <dennis.voltz@wwu.de>
2021-10-05 10:34:48 +0200

    WIP: Add EuiFacetButton with more/less func

--- packages/searchkit-elastic-ui/src/Facets/ListFacet/index.tsx
+++ packages/searchkit-elastic-ui/src/Facets/ListFacet/index.tsx
@@ -1,8 +1,14 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { EuiFacetGroup, EuiTitle, EuiFacetButton } from '@elastic/eui'
 import { useSearchkit, FilterLink, FilterLinkClickRef } from '@searchkit/client'
 
 export const ListFacet = ({ facet, loading }) => {
+  /*FIXME Read from RefinementSelectFacet prop?"*/
+  const elementsFromConfig = 4
+  const difference = 2
+
+  const [elements, setElements] = useState(elementsFromConfig)
+
   const api = useSearchkit()
 
   const entries = facet.entries.map((entry) => {
@@ -26,12 +32,38 @@ export const ListFacet = ({ facet, loading }) => {
     )
   })
 
+  /* Prepare conditions for more/less buttons"*/
+  const loadedEntries = entries.length
+  const showMore = elements < loadedEntries
+  const showLess = elements <= loadedEntries && elements > difference
+  entries.length = elements
+
   return (
     <>
       <EuiTitle size="xxs">
         <h3>{facet.label}</h3>
       </EuiTitle>
       <EuiFacetGroup>{entries}</EuiFacetGroup>
+      {showMore && (
+        <EuiFacetButton
+          aria-label="show more"
+          onClick={() => {
+            setElements(elements + difference)
+          }}
+        >
+          more
+        </EuiFacetButton>
+      )}
+      {showLess && (
+        <EuiFacetButton
+          aria-label="show less"
+          onClick={() => {
+            setElements(elements - difference)
+          }}
+        >
+          less
+        </EuiFacetButton>
+      )}
     </>
   )
 }
