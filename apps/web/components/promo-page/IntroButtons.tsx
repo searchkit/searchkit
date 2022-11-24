959b15dcd6b6147d5da53ee88c5f6bdea84d6e5d
Joseph McElroy <phoey1@gmail.com>
2022-11-24 21:20:10 +0000

    fix positioning of buttons

--- apps/web/components/promo-page/IntroButtons.tsx
+++ apps/web/components/promo-page/IntroButtons.tsx
@@ -6,9 +6,7 @@ export const IntroButtons = () => {
       <div className="rounded-md shadow">
         <a className="w-full flex items-center no-underline justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:border-gray-500 focus:shadow-outline-white transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10" href="/docs/getting-started">Get Started</a>
       </div>
-      <div className="rounded-md shadow ml-3">
-        <a className="w-full flex items-center no-underline justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100  hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 border-gray-900" href="/demo">View Demo</a>
-      </div>
+      <div className="rounded-md shadow mt-3 md:mt-0 md:ml-3"><a className="w-full flex items-center no-underline justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100  hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 border-gray-900" href="/demo">View Demo</a></div>
     </div>
   )
 }
