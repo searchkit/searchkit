bfd27b3d08131b410a83c938df8f1f325a46076c
Joseph McElroy <phoey1@gmail.com>
2022-11-24 21:41:09 +0000

    update meta tags

--- apps/web/theme.config.js
+++ apps/web/theme.config.js
@@ -24,8 +24,14 @@ export default {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Searchkit is an open source library which helps you build a great search experience with Elasticsearch." />
+      <meta name="description" content="Searchkit is an open source library which helps you build a great search experience with Elasticsearch and Instantsearch." />
       <meta name="og:title" content="Searchkit" />
+      <meta name="keywords" content="Elasticsearch, UI Library for Elasticsearch, React Search UI, Nodejs, Search UI components, NextJS Search, Elasticsearch Search UI, Instantsearch" />
+      <meta property="og:url" content="https://beta.searchkit.co"/>
+      <meta property="twitter:title" content="Searchkit - UI Library for Elasticsearch"/>
+      <meta property="twitter:description" content="Searchkit is an open source library which helps you build a great search experience with Elasticsearch and Instantsearch."/>
+      <meta property="twitter:url" content="summary_large_image"/>
+      <meta name="google-site-verification" content="nYA9IOgFmYP8znklDbiAGOx8fRXGvpQBX6NQmIKRwMA" />
       <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c98b302ea3bb4a33a012a7ef0ab3e240"}'></script>
     </>
   ),
