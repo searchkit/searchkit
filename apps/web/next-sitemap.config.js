5fc6e7ea6f9ebaf21e9094b7173d22e0ec4b73f7
Joseph McElroy <phoey1@gmail.com>
2023-01-20 14:12:37 +0000

    updates to sitemap

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: 'https://searchkit.co',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
