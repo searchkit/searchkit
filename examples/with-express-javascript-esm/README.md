c13255870a11551f12f3bd11f1caa24fc9a76782
Joseph McElroy <phoey1@gmail.com>
2023-03-04 13:16:14 +0000

    fix esm exports + types (#1230)
    * fix esm exports
    
    * add examples of esm projects for API
    
    * add examples of esm /cjs projects for API
    
    * clean up configuration
    
    * update ESM package exports

---
+++ examples/with-express-javascript-esm/README.md
@@ -0,0 +1,29 @@
+### Searchkit Express
+
+Example of using Searchkit with Express.js to proxy frontend search requests to Elasticsearch.
+
+This uses ESM modules and is compatible with Node.js 14+.
+
+### Setup
+
+1. Install dependencies
+
+```
+npm install
+```
+
+2. Update the Searchkit configuration in `index.js` with the `connection` and `search_settings` for your search experience.
+
+3. Start server. This will start the server on port 3001.
+
+```
+npm start
+```
+
+4. Update the `url` in `@searchkit/instantsearch-client` configuration to point to the server
+
+```
+const searchClient = SearchkitInstantSearchClient({
+  url: "http://localhost:3001/api/search"
+})
+```
---
+++ examples/with-express-typescript-esm/src/index.ts
@@ -0,0 +1,49 @@
+import express, { Application, Request, Response } from 'express'
+import Client, { SearchkitConfig } from '@searchkit/api'
+import 'isomorphic-unfetch'
+
+const app = express()
+
+const config: SearchkitConfig = {
+  connection: {
+    host: 'https://commerce-demo.es.us-east4.gcp.elastic-cloud.com:9243',
+    apiKey: 'a2Rha1VJTUJMcGU4ajA3Tm9fZ0Y6MjAzX2pLbURTXy1hNm9SUGZGRlhJdw=='
+  },
+  search_settings: {
+    highlight_attributes: ['title'],
+    search_attributes: [{ field: 'title', weight: 3 }, 'actors', 'plot'],
+    result_attributes: ['title', 'actors', 'poster', 'plot'],
+    facet_attributes: [
+      'type',
+      { attribute: 'actors', field: 'actors.keyword', type: 'string' },
+      'rated',
+      { attribute: 'imdbrating', type: 'numeric', field: 'imdbrating' },
+      { attribute: 'metascore', type: 'numeric', field: 'metascore' }
+    ],
+    sorting: {
+      default: {
+        field: '_score',
+        order: 'desc'
+      },
+      _rated_desc: {
+        field: 'rated',
+        order: 'desc'
+      }
+    },
+    snippet_attributes: ['plot'],
+    query_rules: []
+  }
+}
+
+const apiClient = Client(config)
+
+app.use(express.json())
+
+app.post('/api/search', async function (req, res) {
+  const response = await apiClient.handleRequest(req.body)
+  res.send(response)
+})
+
+app.listen(3001, () => {
+  console.log('Server running on port 3000')
+})
